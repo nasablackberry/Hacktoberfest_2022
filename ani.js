d6155804a76222bd8ac46354af64a7c0332f026e
AdityaKURMI <94731548+AdityaKURMI@users.noreply.github.com>
2022-10-17 06:58:10 -0700

    moving_car   hactoberfest

---
+++ ani.js
@@ -0,0 +1,5 @@
+var audioElement = document.createElement('audio')
+audioElement.setAttribute('src','sound.mp3')
+
+audioElement.loop = true;
+audioElement.play();
---
+++ kk.cpp
@@ -0,0 +1,17 @@
+// C++ program to demonstrate the
+// boilerplate code
+#include <iostream>
+using namespace std;
+  
+// Driver Code
+int main()
+{
+    int a;
+    cin >> a;
+    int ar[a];
+
+
+
+
+    return 0;
+}
